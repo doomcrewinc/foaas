a9ab07df85110141073e09d449c0117461825240
doomcrewinc <oilfield101@gmail.com>
2025-05-03 20:15:07 -0600

    Remove unneeded /; FUCK YOU /

--- spec/renderers/html_spec.js
+++ spec/renderers/html_spec.js
@@ -38,7 +38,7 @@ describe('HTML Renderer', function () {
       }
       renderer.render(req, res)
 
-      return expect(res.send).toHaveBeenCalledWith('<!DOCTYPE html> <html> <head> <title>FOAAS - ONE TWO</title> <meta charset="utf-8"> <meta property="og:title" content="ONE TWO"> <meta property="og:description" content="ONE TWO"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link href="//cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> </head> <body style="margin-top:40px;"> <div class="container"> <div id="view-10"> <div class="hero-unit"> <h1>ONE</h1> <p><em>TWO</em></p> </div> </div> <p style="text-align: center"><a href="https://foaas.io/">foaas.io</a></p> </div> </body> </html>')
+      return expect(res.send).toHaveBeenCalledWith('<!DOCTYPE html> <html> <head> <title>FOAAS - ONE TWO</title> <meta charset="utf-8"> <meta property="og:title" content="ONE TWO"> <meta property="og:description" content="ONE TWO"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link href="//cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> </head> <body style="margin-top:40px;"> <div class="container"> <div id="view-10"> <div class="hero-unit"> <h1>ONE</h1> <p><em>TWO</em></p> </div> </div> <p style="text-align: center"><a href="https://foaas.io">foaas.io</a></p> </div> </body> </html>')
     })
   })
 })
